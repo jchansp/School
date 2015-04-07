8f28b29e1524f36b2a4dd8402aa563bd2e792335
jchansp <jciansp@hotmail.com>
2015-04-07 13:38:08 +0200

    StudentCreateTest and TeacherCreateTest created

---
+++ School/Entities.Tests/Teacher.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Entities.Tests
+{
+    /// <summary>
+    ///     Summary description for Teacher
+    /// </summary>
+    [TestClass]
+    public class Teacher
+    {
+        /// <summary>
+        ///     Gets or sets the test context which provides
+        ///     information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TeacherCreateTest()
+        {
+            var teacher = new Entities.Teacher();
+        }
+
+        #region Additional test attributes
+
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+
+        #endregion
+    }
+}
