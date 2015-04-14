55dfa73d5fe3d411862f8a0224daea4bf23b9a30
jchansp <jciansp@hotmail.com>
2015-04-14 22:51:13 +0200

    Student.Persist created

--- School/Entities/Student.cs
+++ School/Entities/Student.cs
@@ -1,15 +1,48 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Entities
 {
     public class Student : Person
     {
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+
         internal Student()
+        {
+            Persist();
+        }
+
+        public Student(Guid id, string firstName, Country country)
+            : base(id, firstName, country)
         {
         }
 
-        public Student(Guid id, string firstName, Country country) : base(id, firstName, country)
+        internal new void Persist()
         {
+            Persist(Id);
+        }
+
+        private void Persist(Guid id)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                using (var sqlCommand = new SqlCommand("SetStudents", sqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    sqlConnection.Open();
+                    var dataTable = new DataTable();
+                    dataTable.Columns.Add("Id", typeof(Guid));
+                    dataTable.Rows.Add(id);
+                    var sqlParameter = sqlCommand.Parameters.AddWithValue("@Student", dataTable);
+                    sqlParameter.SqlDbType = SqlDbType.Structured;
+                    sqlParameter.TypeName = "Student";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
