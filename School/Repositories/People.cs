56667ac2cdc354c07ef79d6e83bf41559c42dc6d
jchansp <jciansp@hotmail.com>
2015-04-16 17:59:13 +0200

    Lost files

---
+++ School/Repositories/People.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repositories
+{
+    public static class People
+    {
+        private static readonly string ConnectionString =
+            ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+
+        public static void Persist(IEnumerable<Person> people)
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof (Guid));
+            dataTable.Columns.Add("FirstName", typeof (string));
+            dataTable.Columns.Add("CountryCode", typeof (string));
+            foreach (var person in people)
+            {
+                dataTable.Rows.Add(person.Id, person.FirstName, person.CountryId);
+            }
+            Persist(dataTable);
+        }
+
+        private static void Persist(DataTable dataTable)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                using (var sqlCommand = new SqlCommand("SetPeople", sqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    sqlConnection.Open();
+                    var sqlParameter = sqlCommand.Parameters.AddWithValue("@Person", dataTable);
+                    sqlParameter.SqlDbType = SqlDbType.Structured;
+                    sqlParameter.TypeName = "Person";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Persist(Person person)
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof (Guid));
+            dataTable.Columns.Add("FirstName", typeof (string));
+            dataTable.Columns.Add("CountryCode", typeof (string));
+            dataTable.Rows.Add(person.Id, person.FirstName, person.CountryId);
+            Persist(dataTable);
+        }
+    }
+}
---
+++ School/Repositories/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Repositories
+{
+    public class Person
+    {
+        private readonly string _countryId;
+        private readonly string _firstName;
+        private readonly Guid _id;
+
+        public Person(Guid id, string firstName, string countryId)
+        {
+            _id = id;
+            _firstName = firstName;
+            _countryId = countryId;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        public string CountryId
+        {
+            get { return _countryId; }
+        }
+    }
+}
